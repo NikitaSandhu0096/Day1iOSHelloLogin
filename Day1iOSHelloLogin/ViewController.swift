ccbc7e79c735099775b2090e1ffcd3e6c2d0ea61
Nikita Sandhu <sandhunikita0096@gmail.com>
2020-02-28 21:12:27 -0500

    login working

--- Day1iOSHelloLogin/ViewController.swift
+++ Day1iOSHelloLogin/ViewController.swift
@@ -32,7 +32,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         print("viewDidLoad")
-        lbWelcome.text = "Welcome to iOS Programming"
+      //  lbWelcome.text = "Welcome to iOS Programming"
         
         print(lbWelcome.text)
     }
